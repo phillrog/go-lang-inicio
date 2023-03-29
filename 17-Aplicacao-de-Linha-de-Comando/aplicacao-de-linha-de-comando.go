db754ce485298000042e29036582303ac10e311b
phillrog <phillrog@hotmail.com>
2023-03-28 21:17:10 -0300

    Implementação da busca dos ips

--- 17-Aplicacao-de-Linha-de-Comando/aplicacao-de-linha-de-comando.go
+++ 17-Aplicacao-de-Linha-de-Comando/aplicacao-de-linha-de-comando.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go-lang-inicio/17-Aplicacao-de-Linha-de-Comando/app"
 	"log"
 	"os"
@@ -9,7 +8,7 @@ import (
 
 func main() {
 	aplicacao := app.Gerar()
-	fmt.Println(aplicacao)
+
 	if err := aplicacao.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
--- 17-Aplicacao-de-Linha-de-Comando/app/app.go
+++ 17-Aplicacao-de-Linha-de-Comando/app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -9,7 +13,34 @@ func Gerar() *cli.App {
 	app := &cli.App{
 		Name:  "Aplicação de Linha de Comando",
 		Usage: "busca IPs e Nomes de Servidor na Internet",
+		Commands: []*cli.Command{
+			{
+				Name:  "ip",
+				Usage: "busca IPs de endereços na internet",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "host",
+						Value: "devbook.com.br",
+					},
+				},
+				Action: buscarIps,
+			},
+		},
 	}
 
 	return app
+}
+
+func buscarIps(ctx *cli.Context) error {
+	host := ctx.String("host")
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+	return erro
 }
